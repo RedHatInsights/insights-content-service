a3aa1ce1d442f212a14056295aca2918aeb0f9dc
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-05-18 13:42:28 +0200

    Elementary tests for server module

--- server/server_test.go
+++ server/server_test.go
@@ -15,3 +15,70 @@ limitations under the License.
 */
 
 package server_test
+
+import (
+	"context"
+	"net/http"
+	"testing"
+	"time"
+
+	"github.com/rs/zerolog"
+
+	"github.com/RedHatInsights/insights-content-service/server"
+	"github.com/RedHatInsights/insights-content-service/tests/helpers"
+)
+
+var config = server.Configuration{
+	Address:     ":8080",
+	APIPrefix:   "/api/test/",
+	APISpecFile: "openapi.json",
+	Debug:       true,
+	UseHTTPS:    false,
+}
+
+func init() {
+	zerolog.SetGlobalLevel(zerolog.WarnLevel)
+}
+
+func checkResponseCode(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+	}
+}
+
+func TestServerStart(t *testing.T) {
+	helpers.RunTestWithTimeout(t, func(t *testing.T) {
+		s := server.New(server.Configuration{
+			// will use any free port
+			Address:   ":0",
+			APIPrefix: config.APIPrefix,
+			Debug:     true,
+		}, nil)
+
+		go func() {
+			for {
+				if s.Serv != nil {
+					break
+				}
+
+				time.Sleep(500 * time.Millisecond)
+			}
+
+			// doing some request to be sure server started successfully
+			req, err := http.NewRequest(http.MethodGet, config.APIPrefix, nil)
+			helpers.FailOnError(t, err)
+
+			response := helpers.ExecuteRequest(s, req, &config).Result()
+			checkResponseCode(t, http.StatusOK, response.StatusCode)
+
+			// stopping the server
+			err = s.Stop(context.Background())
+			helpers.FailOnError(t, err)
+		}()
+
+		err := s.Start()
+		if err != nil && err != http.ErrServerClosed {
+			t.Fatal(err)
+		}
+	}, 5*time.Second)
+}
