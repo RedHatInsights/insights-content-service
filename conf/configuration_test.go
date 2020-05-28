303f4b69c8a74e05f2f4b2c2ed999ffe02752cd3
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-05-28 10:47:17 +0200

    Checking loading broken configuration file

--- conf/configuration_test.go
+++ conf/configuration_test.go
@@ -61,6 +61,15 @@ func TestLoadConfiguration(t *testing.T) {
 	loadProperConfigFile(t)
 }
 
+// TestLoadBrokenConfiguration loads a configuration file for testing
+func TestLoadBrokenConfiguraion(t *testing.T) {
+	os.Clearenv()
+	err := conf.LoadConfiguration("tests/config_improper_format")
+	if err == nil {
+		t.Fatal("Broken configuration file should be detected")
+	}
+}
+
 // TestLoadGroupsConfiguration tests loading the groups configuration sub-tree
 func TestLoadGroupsConfiguration(t *testing.T) {
 	loadProperConfigFile(t)
