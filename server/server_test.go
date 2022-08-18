b43cc2461fe5037ced1ac4bd50f046c6fd04de1c
Juan Díaz <juandiazsuarez0@gmail.com>
2022-08-18 09:26:49 +0200

    Update server_test.go

--- server/server_test.go
+++ server/server_test.go
@@ -18,7 +18,7 @@ package server_test
 
 import (
 	"context"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"os"
 	"testing"
@@ -143,7 +143,7 @@ func TestServeAPISpecOptionsMethod(t *testing.T) {
 // TestServeAPISpecFileError checks the error tests in REST API server handler
 func TestServeAPISpecFileError(t *testing.T) {
 	// openapi.json is really not there
-	dirName, err := ioutil.TempDir("/tmp/", "")
+	dirName, err := io.TempDir("/tmp/", "")
 	helpers.FailOnError(t, err)
 
 	err = os.Chdir(dirName)
