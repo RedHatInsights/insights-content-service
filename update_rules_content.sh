c8847419918c2d18ac695d8559aef88dc30ac88c
Martin Zibricky <matysek@users.noreply.github.com>
2023-03-03 14:26:28 +0100

    test - pr_check not executed
    pr_check shoudl not run when this file is updated.

--- update_rules_content.sh
+++ update_rules_content.sh
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
+
 # Updates the ./rules-content directory with the latest rules to test with
 
 set -exv
