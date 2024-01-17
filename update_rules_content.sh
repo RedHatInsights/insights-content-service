d3939b7ce4beb0a1b06d75207b2e149960e56b53
InsightsDroid <ccx@redhat.com>
2024-01-17 11:45:09 +0000

    Bumped ccx-rules-ocp version

--- update_rules_content.sh
+++ update_rules_content.sh
@@ -25,7 +25,7 @@ function clean_up() {
 trap clean_up EXIT
 
 # Updated with every new ccx-rules-opc release.
-CCX_RULES_OCP_TAG="2024.01.03"
+CCX_RULES_OCP_TAG="2024.01.17"
 
 RULES_REPO="https://gitlab.cee.redhat.com/ccx/ccx-rules-ocp.git"
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
